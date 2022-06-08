1fa377372708db52da52be46061f5b885b71abbf
Rahim313 <106736488+Rahim313@users.noreply.github.com>
2022-06-09 00:04:51 +0530

    Add files via upload

---
+++ Operators_In _python/Cmt_indent.py
@@ -0,0 +1,14 @@
+'''
+x=10
+#print(x)
+y="LnB"
+#print(y)
+z=x*y
+print(z)
+'''
+
+a=5
+if(a==10):
+    print("Its true")
+else:
+    print("Not True")
---
+++ Operators_In _python/Operators_inPY.py
@@ -0,0 +1,89 @@
+#python arithmatic opeators
+a=10
+b=5
+'''
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a**b)
+print(a%b)
+'''
+#Python comparison operator
+'''
+print(a==b)
+print(a!=b)
+print(a<10>b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+'''
+#Python Assignment operator
+'''
+data=10
+print(data)
+data+=10
+print(data)
+data-=10
+print(data)
+data*=10
+print(data)
+data/=10
+print(data)
+data**=7
+print(data)
+data//=10
+print(data)
+data%=5
+print(data)
+'''
+
+#Python Bitwise operator
+'''
+a=0b00111100
+b=0b00001101
+print(bin(a&b))
+print(bin(a|b))
+print(bin(a^b))
+print(bin(~a))
+print(bin(a<<2))
+'''
+
+
+#Python Logical operator
+'''
+a=0b10101
+b=0b11011
+print(a and b)
+print(a or b)
+'''
+
+#Python Membership operator
+'''
+a=[1,2,3,4]
+b=2
+c=7
+res=c in a
+print(c not in a)
+print(res)
+'''
+
+#Python Identity operator
+
+'''
+a=10
+b=20
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+'''
+
+#Python operators precedence - order of execution
+
+# * /  % //
+#is    not is
+#in    not in
